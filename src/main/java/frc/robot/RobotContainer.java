850a5047a10b7b71b15cde0ad39c9bc8d85fbac8
CardinalBotics <trevor.choy.1@gmail.com>
2025-03-12 18:06:11 -0700

    led code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LED;
+import frc.robot.subsystems.LED.LEDState;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final Joystick joystick = new Joystick(0);
+  private final JoystickButton ledIntake = new JoystickButton(joystick, 6);
+  private final JoystickButton ledAlgae = new JoystickButton(joystick, 2);
+  private final JoystickButton ledL4 = new JoystickButton(joystick, 3);
+  private final JoystickButton ledL3 = new JoystickButton(joystick, 4);
+  private final JoystickButton ledL2 = new JoystickButton(joystick, 5);
+  private final JoystickButton ledL1 = new JoystickButton(joystick, 6);
+  private final JoystickButton ledDeepClimb = new JoystickButton(joystick, 7);
+
+  private final LED led = new LED();
+
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    led.bindButton(ledIntake, LEDState.RAINBOW);
+    led.bindButton(ledAlgae, LEDState.TURQUOISE);
+    led.bindButton(ledL4, LEDState.PURPLE);
+    led.bindButton(ledL3, LEDState.BLUE);
+    led.bindButton(ledL2, LEDState.GREEN);
+    led.bindButton(ledL1, LEDState.WHITE);
+    led.bindButton(ledDeepClimb, LEDState.RED);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
